3efb105f0b620ced165d81d7d9ec51926d65c846
antuanuran <anton.uranov@yandex.ru>
2023-12-07 08:18:37 +0300

    1

--- tests/test_file.py
+++ tests/test_file.py
@@ -4,9 +4,6 @@ from unittest import TestCase
 class MultiTest(TestCase):
     def test_proverka(self):
         res = 3 * 2
-        self.assertEqual(res, 6)
+        self.assertEqual(res, 7)
 
 
-def test_Multi():
-    res = 3 * 2
-    assert res == 6
