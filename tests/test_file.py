e90d202da7cd640e3bc020277f034406dd4c0275
antuanuran <anton.uranov@yandex.ru>
2023-12-14 10:27:21 +0300

    1

--- tests/test_file.py
+++ tests/test_file.py
@@ -5,3 +5,11 @@ class MultiTest(TestCase):
     def test_1(self):
         res = 3 * 2
         self.assertEqual(res, 6)
+
+
+# assertEqual(a, b)	            Проверяет, что a == b
+# assertNotEqual(a, b)	        Проверяет, что a != b
+# assertTrue(x)	                Проверяет, что значение x истинно
+# assertFalse(x)	                Проверяет, что значение x ложно
+# assertIn(элемент, список)	    Проверяет, что элемент входит в список
+# assertNotIn(элемент, список)    Проверяет, что элемент не входит в спис
