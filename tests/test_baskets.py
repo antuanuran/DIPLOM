bfd93eb598592b5c09fda6308eda487f10aed826
Antuan_Uranov <uranovanton@gmail.com>
2023-12-15 12:04:08 +0300

    cicd

--- tests/test_baskets.py
+++ tests/test_baskets.py
@@ -1,9 +1,18 @@
-import pytest
+# import pytest
+#
+# from apps.users.models import User
+#
+#
+# @pytest.mark.django_db
+# def test_getbasket():
+#     user = User.objects.create_user("qwerqreweq@gmail.com")
+#     assert user.is_vendor is False
 
-from apps.users.models import User
+
+from unittest import TestCase
 
 
-@pytest.mark.django_db
-def test_getbasket():
-    user = User.objects.create_user("qwerqreweq@gmail.com")
-    assert user.is_vendor is False
+class MultiTest(TestCase):
+    def test_1(self):
+        res = 3 * 3
+        self.assertEqual(res, 9)
